403d8108a158604fe6650145f21514caec0941fe
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-05 13:30:23 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   result = {}
   languages.each do |style, lang|
     lang.each do |lang, type|
-
+binding.pry
        result[lang] = {
           :type => lang[:type],
           :style => [style]
